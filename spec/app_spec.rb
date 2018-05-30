611008a1460e20adaf8bf2c4f57bb988178291e9
Meltens <irimamekun@gmail.com>
2018-05-30 20:46:05 +0900

    Update: app_spec.rb

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,21 +2,24 @@
 
 require 'spec_helper'
 require 'pry'
-# require File.expand_path '../spec_helper.rb', __FILE__
+require File.join(File.dirname(__FILE__), '..', 'models.rb')
+
 
 describe 'Sinatra' do
+
   context 'GET to /' do
     let(:response) { get '/' }
-    it 'The API needs to response 200' do
-      expect(response.status).to eq 200
-    end
+    it 'The API needs to response 200' do expect(response.status).to eq 200 end
   end
 
   context 'POST to /sign_up' do
-    data = '{"mail": "sample@sample.com", "password": "hogehoge"}'
-    let(:response) {post '/sign_up' , data.to_json} #, "CONTENT_TYPE" => "application/json" 
-    it 'Create an account' do
-      expect(response.status).to eq 200
-    end
+    # POST Data
+    data = "{'mail': 'hoge@sample.com', 'password': 'hogehoge'}"
+    header = {'Content-Type' => 'application/json'}
+    let(:response) {post '/sign_up' , data.to_json, header}
+
+    it 'The signup needs to response' do expect(response.status).to eq 201 end
+    it 'Create an account' do expect(response.body) end
+
   end
 end
