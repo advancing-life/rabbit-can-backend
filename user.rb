eef9a3cdbbe66d0a40bca0ed50306dbcb5200c4e
flmil <reiserver629412@gmail.com>
2018-06-03 12:51:57 +0900

    Setting login
    status code
    
    edit status code sign in
    
    result-json id -> u_id
    
    add sign in
    
    add login
    
    login complete

--- user.rb
+++ user.rb
@@ -1,28 +1,28 @@
 require 'securerandom'
 
 class UserContents
-  def new_create_user(ns_mail,ns_password)#新規ユーザーの登録
-    puts "UserContents.new.new_create_user--------------------------------------------------------------"
-    puts ns_mail
-    puts ns_password
-    @user = User.create(
+  def new_create_user(ns_mail,ns_password,ns_name)#新規ユーザーの登録
+    user = User.create(
       u_id:"#{SecureRandom.hex(64)}",
+      name:ns_name,
       mail:ns_mail,
       password_digest:ns_password,
     )
 
-    if @user.persisted?
-      puts "登録完了"
-      #sessionの保存をさせる
-    end
+    return user
   end
   def oauth_user(is_mail,is_password)#ユーザーのログイン認証
-    puts "UserContents.new.oauth_user--------------------------------------------------------------"
-    puts is_mail
-    puts is_password
-    user = User.find_by(mail: is_mail)
-    if user.password && is_password
-      #sessionの保存をさせる
+    begin
+      user = User.find_by(mail: is_mail)
+
+      if user.password_digest == is_password
+        #sessionの保存をさせる
+        return user
+      else
+        #パスワードが違います
+        return false
+      end
+    rescue
     end
   end
 end
